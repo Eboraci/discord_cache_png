ccb6db53f2fd2605479efef1cdca2979adaffe45
Pringles <theyork342@hotmail.com>
2019-02-26 17:43:24 -0300

    Update discord_cache_png.py
    O script não se reescreve mais.

--- discord_cache_png.py
+++ discord_cache_png.py
@@ -7,7 +7,7 @@ cont = 0
 if "Cache" in os.getcwd(): #Verifica se esta no dir correto
     os.getcwd()    # Pega o dir atual
     for fileName in os.listdir("."):
-        if "data" in fileName or "index" in fileName:
+        if "data" in fileName or "index" in fileName or "discord_cache_png.py" in fileName:
             continue     #Filtra arquivos que não sejam fotos, caso não seja, ele pula para o proximo loop
         else:
             print("Arquivo renomeado: {}".format(fileName))
