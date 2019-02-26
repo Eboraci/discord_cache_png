0bd99facde48525f2473bf3332fab7222bbf5c9f
Pringles <theyork342@hotmail.com>
2019-02-26 17:33:59 -0300

    Update discord_cache_png.py

--- discord_cache_png.py
+++ discord_cache_png.py
@@ -4,14 +4,15 @@ import os
 
 cont = 0
 
-os.getcwd()    # Pega o dir atual
-
-for fileName in os.listdir("."):
-    if "data" in fileName or "index" in fileName:
-        continue     #Filtra arquivos que não sejam fotos, caso não seja, ele pula para o proximo loop
-    else:
-        print("Arquivo renomeado: {}".format(fileName))
-        os.rename(fileName, fileName + ".png")   #Renomeia para a extensão PNG
-        cont += 1
-
-print("Foram renomeados {} arquivos.".format(cont))
+if "Cache" in os.getcwd(): #Verifica se esta no dir correto
+    os.getcwd()    # Pega o dir atual
+    for fileName in os.listdir("."):
+        if "data" in fileName or "index" in fileName:
+            continue     #Filtra arquivos que não sejam fotos, caso não seja, ele pula para o proximo loop
+        else:
+            print("Arquivo renomeado: {}".format(fileName))
+            os.rename(fileName, fileName + ".png")   #Renomeia para a extensão PNG
+            cont += 1
+    print("Foram renomeados {} arquivos.".format(cont))
+else:
+    print("Estou no diretorio incorreto!")
